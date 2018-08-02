bf818e22a860c54252a4f57be319103dc847f5ca
Bradley Lowekamp <blowekamp@mail.nih.gov>
2018-08-02 16:14:20 -0400

    Revert "ENH: Improve numeric computation of linear interpolation for Resample"
    This reverts commit 9c448f1b9446e70ee661cd585d5ac7098eeeb5d4.
    
    Change-Id: I7f2b85485fca5894287736c1fe05117399ea2e24

--- Modules/Filtering/ImageGrid/include/itkResampleImageFilter.hxx
+++ Modules/Filtering/ImageGrid/include/itkResampleImageFilter.hxx
@@ -399,17 +399,17 @@ ResampleImageFilter< TInputImage, TOutputImage, TInterpolatorPrecisionType, TTra
 
     IndexValueType scanlineIndex = outIt.GetIndex()[0];
 
-
     while ( !outIt.IsAtEndOfLine() )
       {
 
       // Perform linear interpolation between startIndex and endIndex
-      const double alpha = (scanlineIndex - largestPossibleRegion.GetIndex(0)) / (double)(largestPossibleRegion.GetSize(0));
+      const double alpha = (scanlineIndex - largestPossibleRegion.GetIndex(0) ) / double(largestPossibleRegion.GetSize(0));
+      const double oneMinusAlpha = 1.0 - alpha;
 
-      ContinuousInputIndexType inputIndex( startIndex );
+      ContinuousInputIndexType inputIndex;
       for (unsigned int i = 0; i < ImageDimension; ++i)
         {
-        inputIndex[i] += alpha * ( endIndex[i] - startIndex[i] );
+        inputIndex[i] = startIndex[i] * oneMinusAlpha + endIndex[i] * alpha;
         }
 
       PixelType  pixval;
